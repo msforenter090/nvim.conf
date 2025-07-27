e6830fe4889439a7b5dfc6b8840483cc153051c3
msforenter090 <msforenter090@gmail.com>
2025-07-27 18:40:50 +0200

    ave

--- init.lua
+++ init.lua
@@ -36,6 +36,7 @@ require("config.lazy").setup({
                     "shfmt",
 
                     -- Python
+                    "python-lsp-server",
                 },
             })
         end,
@@ -62,6 +63,38 @@ require("config.lazy").setup({
         config = function()
             require("mason-lspconfig").setup()
             local lspconfig = require("lspconfig")
+
+            lspconfig.pylsp.setup({
+                capabilities = capabilities,
+                on_attach = function(client, bufnr)
+                    local opts = { noremap = true, silent = true, buffer = bufnr }
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    if client:supports_method("textDocument/completion") then
+                        vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
+                        vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
+                        vim.keymap.set("i", "<C-Space>", function()
+                            vim.lsp.completion.get()
+                        end)
+                    end
+                end,
+                settings = {
+                    pylsp = {
+                        plugins = {
+                            pyflakes = { enabled = true },
+                            pycodestyle = { enabled = true },
+                            pylint = { enabled = false },
+                            mccabe = { enabled = false },
+                            yapf = { enabled = false },
+                            rope_completion = { enabled = true },
+                            jedi_completion = { fuzzy = true },
+                            jedi_definition = { follow_builtin_imports = true },
+                        },
+                    },
+                },
+            })
+
             lspconfig.bashls.setup({
                 on_attach = function(client, bufnr)
                     if client:supports_method("textDocument/completion") then
